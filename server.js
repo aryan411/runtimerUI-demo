c1fe1c319a68d3e20a46881901eb429607cf2e7b
Aryan Patel <appatel@augmentedtechlabs.com>
2022-07-17 22:07:08 -0400

    path changes

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-import express from 'express';
-import path from 'path';
+const express = require("express");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Client'));
+app.use(express.static(__dirname + "/dist/Client"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Client/'}),
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname + "/dist/Client/index.html"))
 );
 
 // Start the app by listening on the default Heroku port
